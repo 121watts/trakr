620872903697708bbbe2b9db031805e3f39fcbac
Andrew Watkins <watts@influxdb.com>
2018-04-01 15:20:46 -0700

    Add react router and firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
 
-const logo = require('./logo.svg')
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Trackr</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p className="App-intro">To get started, write some code</p>
       </div>
     )
   }
